d8adb13ec4ec78e3abf0990ee62373e67922c5bf
Miriam Ahlberg <tna12mah@student.lu.se>
2016-09-30 19:09:19 +0200

    Trying to write HangmanTest

---
+++ src/HangmanTest.java
@@ -0,0 +1,16 @@
+import org.unit.Test;
+import java.util.*;
+ 
+public class HangmanTest {
+	
+	public static void main(String[] args) {
+		testWindow();
+	}
+	
+	public static boolean testWindow() {
+		InputWindow window = new InputWindow();
+		//assertTrue(window.getString().equals("hej"));
+		//Assert.assertTrue(1 == 1);
+		return true;
+	}
+}
---
+++ src/InputWindow.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+
+public class InputWindow extends JOptionPane {
+	private String s;
+	
+	public InputWindow() {
+		s = showInputDialog("Enter a word");
+	}
+	
+	public String getString(){
+		return s;
+	}
+
+}
