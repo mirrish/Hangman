4e2d1b7e247ebcf2d7e6c9466c0da8964c72540a
Miriam Ahlberg <tna12mah@student.lu.se>
2016-11-20 12:10:22 +0100

    Separated into main and test folders

---
+++ src/test/HangmanTest.java
@@ -0,0 +1,17 @@
+package test;
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+
+public class HangmanTest {
+
+    @Test
+    public void testMain() {
+        fail("Not yet implemented");
+    }
+
+}
+
+//Test tutorial:
+//https://courses.cs.washington.edu/courses/cse143/11wi/eclipse-tutorial/junit.shtml#creating
